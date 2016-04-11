3bc39a4c00eee7752bf0f083defa21af60ec11f6
Jelmer Scholte <jelmerscholte1@hotmail.com>
2016-04-11 17:18:21 +0200

    beginslider

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -1,20 +1,60 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Stichting Buitenbeesten</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="../resources/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="homepagejs.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"> </script>
+    <script src="http://malsup.github.com/jquery.cycle2.js"></script>
+</head>
+<body>
+    <div id="container">
+        <nav id ="nav">
+            <ul id="sectionlist">
+              <li class='navlistobj'><a href='#'>Doelstelling</a></li>
+              <li class='navlistobj'><a href='#'>Bestuur</a></li>
+              <li class='navlistobj'><a href='#'>Financieel</a></li>
+              <li class='navlistobj'><a href='#'>Contact</a></li>
+            </ul>
+        </nav>
+            
+        <div id = "header">
+<!-- slideshow with one image -->
+<div class="cycle-slideshow" 
+        data-cycle-fx="scrollHorz" 
+        data-cycle-timeout="2000"
+    >
+    <div class="cycle-caption"></div>
+    <img src="http://malsup.github.io/images/p1.jpg">
+</div>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
+<div class=center>
+    <button>Add More Images</button>
+</div>
 
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">
-                    <?php echo $name; ?>
+<!-- script to add more images at a later time -->
+<script>
+var images = [
+    '<img src="http://malsup.github.io/images/p2.jpg">',
+    '<img src="http://malsup.github.io/images/p3.jpg">',
+    '<img src="http://malsup.github.io/images/p4.jpg">'
+];
 
-                </div>
-            </div>
+$('button').one('click', function() {
+    for (var i=0; i < images.length; i++) {
+        $('.cycle-slideshow').cycle('add', images[i]);
+    }
+    $(this).prop('disabled', true)
+})
+</script>
         </div>
-    </body>
+        <div id = "doelstelling"></div>
+        <div id = "bestuur"></div>
+        <div id = "financieel"></div>
+        <div id = "contact"></div>
+        <div id="footer"></div>
+    </div>
+</body>
 </html>
