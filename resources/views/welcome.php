2903db30052b14caf3db65f51f0c2dd15d1a73bf
Jelmer Scholte <jelmerscholte1@hotmail.com>
2016-04-16 14:17:53 +0200

    doelstelling section+scrolaaa

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -5,8 +5,8 @@
     <title>Stichting Buitenbeesten</title>
     <link rel="stylesheet" href="assets/css/styles.css">
     <link href='https://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
-    <script src="../resources/vendor/jquery/dist/jquery.min.js"></script>
-    <script src="homepagejs.js"></script>
+    <script src="../resources/assets/vendor/jquery/dist/jquery.min.js"></script>
+    <script src="../resources/assets/homepagejs.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"> </script>
     <script src="http://malsup.github.com/jquery.cycle2.js"></script>
 </head>
@@ -14,26 +14,42 @@
     <div id="container">
         <nav id ="nav">
             <ul id="sectionlist">
-              <li class='navlistobj'><a href='#'>Doelstelling</a></li>
-              <li class='navlistobj'><a href='#'>Bestuur</a></li>
+              <li class='navlistobj'><a href='#doelstelling'>Doelstelling</a></li>
+              <li class='navlistobj'><a href='#bestuur'>Bestuur</a></li>
               <li class='navlistobj'><a href='#'>Financieel</a></li>
               <li class='navlistobj'><a href='#'>Contact</a></li>
             </ul>
         </nav>
             
         <div id = "header">
+       <img id="logo" src="../resources/assets/images/logo_buitenbeesten.png" alt="stiching buitenbeesten logo"></img>
 <!-- slideshow with one image -->
-<div class="cycle-slideshow" 
-        data-cycle-fx="scrollHorz" 
-        data-cycle-timeout="2000"
-        data-cycle-auto-height=false
-    >
-    <div class="cycle-caption"></div>
-    <img src="http://malsup.github.io/images/p1.jpg">
-</div>
+            <div class="cycle-slideshow" 
+                    data-cycle-fx="scrollHorz" 
+                    data-cycle-timeout="2000"
+                    data-cycle-auto-height=false
+                >
+                <div class="cycle-caption"></div>
+                <img src="http://malsup.github.io/images/p1.jpg">
+            </div>
+<!--end slidehow-->
         </div>
-        <div id = "doelstelling"></div>
-        <div id = "bestuur"></div>
+        <div id = "doelstelling"> 
+            <h1>Doelstelling</h1>
+        <div id="doelstellingtekst">
+        <h2>Doelstellingen Stiching Buitenbeesten</h2>
+        <p id='doelp'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div id="beleidsplantekst">
+            <h2>Beleidsplan Stiching Buitenbeesten</h2>
+            <p id='doelp'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        <hr id="einddoelstelling"></hr>
+    </div>
+       
+        <div id = "bestuur">
+            <h1>Bestuur</h1>
+        </div>
         <div id = "financieel"></div>
         <div id = "contact"></div>
         <div id="footer"></div>
