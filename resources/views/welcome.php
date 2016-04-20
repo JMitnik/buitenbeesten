a18b2dc957398fc847542243778a8f6edc33839d
Jelmer Scholte <jelmerscholte1@hotmail.com>
2016-04-20 20:00:43 +0200

    chef

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -30,27 +30,52 @@
                     data-cycle-auto-height=false
                 >
                 <div class="cycle-caption"></div>
-                <img src="http://malsup.github.io/images/p1.jpg">
+                <img src="../resources/assets/images/IMG_1180.jpeg">
             </div>
 <!--end slidehow-->
         </div>
-        <div id = "doelstelling"> 
-            <h1>Doelstelling</h1>
-        <div id="doelstellingtekst">
-        <h2>Doelstellingen Stiching Buitenbeesten</h2>
-        <p id='doelp'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div id = "doelstelling" class="clearfix"> 
+            <h1>Waar staat de naam stichting BuitenBeesten voor?</h1>
+                   <p id='doelp'>Kinderen die om welke reden dan ook niet passen binnen de setting van een regulier manege/kinderboerderij, een plek te bieden om te leren omgaan met met allerlei boerderijdieren en de daarbij horende verzoring.
+De kernactiviteit  is het zoveel als mogelijk zelfstandig maken van deze kinderen door een leeromgeving aan te bieden met dieren in het algemeen en in het bijzonder met paarden, olv. gekwalificeerd personeel.
+</p>
+            <h2>Doelstellingen Stichting Buitenbeesten</h2>
+            <p id='doelp'>Kinderen die om welke reden dan ook niet passen binnen de setting van een regulier manege/kinderboerderij, een plek te bieden om te leren omgaan met met allerlei boerderijdieren en de daarbij horende verzoring.
+De kernactiviteit  is het zoveel als mogelijk zelfstandig maken van deze kinderen door een leeromgeving aan te bieden met dieren in het algemeen en in het bijzonder met paarden, olv. gekwalificeerd personeel.
+</p>
+        <h2>Beleidsplan Stichting Buitenbeesten</h2>
+                   <p id='doelp'>Ons beleidsplan kun je hier vinden: LONK</p>
         </div>
-        <div id="beleidsplantekst">
-            <h2>Beleidsplan Stiching Buitenbeesten</h2>
-            <p id='doelp'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    <div id = "bestuur">
+      <h1>Bestuur</h1>
+       <div id="bestuurcontent" class="clearfix">
+       <div class="thumbfloater">
+        <div class="thumbnail">
+          <img src="http://ms00.mask9.com/mt0x0000/10381/image/201003/author-sjon-poster-mask9.jpg" alt="Image" />
+
         </div>
-        <hr id="einddoelstelling"></hr>
+          <p>Voorzitter: Gwan Ho</p>
+        </div>
+       <div class="thumbfloater">
+        <div class="thumbnail">
+          <img src="http://ms00.mask9.com/mt0x0000/10381/image/201003/author-sjon-poster-mask9.jpg" alt="Image" />
+
+        </div>
+          <p>Secretaris: Monique Geurts</p>
+        </div>
+       <div class="thumbfloater">
+        <div class="thumbnail">
+          <img src="http://ms00.mask9.com/mt0x0000/10381/image/201003/author-sjon-poster-mask9.jpg" alt="Image" />
+        </div>
+          <p>Penningmeester: Vincent Zumpolle</p>
+        </div>
     </div>
-       
-        <div id = "bestuur">
-            <h1>Bestuur</h1>
+        <p id='bestuurp'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+    </div>
+
+        <div id = "financieel">
+            <h1>Financieel</h1>
         </div>
-        <div id = "financieel"></div>
         <div id = "contact"></div>
         <div id="footer"></div>
     </div>
