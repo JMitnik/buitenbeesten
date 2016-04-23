b04032a997990c04a743bd3e37c88995b3b646a2
Jonathan Mitnik <jmitnik@gmail.com>
2016-04-23 16:55:50 +0200

    Doelstelling

---
+++ resources/views/doelstellingen.php
@@ -0,0 +1,86 @@
+    <div id="doelstelling" class="clearfix section">
+        <div class="subsection">
+            <h1>Waar staat de naam stichting BuitenBeesten voor?</h1>
+            <p>Alles wat we met kinderen buiten op de boerderij samen met de beesten kunnen doen!
+            Samen met gekwalificeerde collega's vormen we een superteam om kinderen een fijne leerzame middag te geven in een rustige en uitdagende omgeving. We kijken naar wat het kind nodig heeft, in overleg met de ouders, en stellen daarop bepaalde doelen af.
+            </p>
+            <h3>Zoals:</h3>
+            <ul class="clearfix">
+                <li>
+                <span>
+                    <span class="fa-stack fa-lg">
+                        <!-- <i class="fa fa-circle-o fa-stack-2x"></i> -->
+                        <i class="fa fa-smile-o fa-stack-2x" aria-hidden="true"></i>
+                    </span>
+                    <span>Versterken van het zelfbeeld en zelfvertrouwen.</span>
+                </span>
+
+                </li>
+                <li>
+                <span>
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-eye fa-stack-2x" aria-hidden="true"></i>
+                    </span>
+                    <span>Verbetering van sesorische informatieverwerking.</span>
+                </span>
+                </li>
+                <li>
+                <span>
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-thumbs-o-up fa-stack-2x" aria-hidden="true"></i>
+                    </span>
+                    <span>Versterken en verbeteren van de motoriek.</span>
+                </span>
+                </li>
+            
+                <li>
+                <span>
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-users fa-stack-2x" aria-hidden="true"></i>
+                    </span>
+                    <span>Samenwerken met andere kinderen door stimuleren van de sociaal emotioneel
+                ontwikkelingen. Structuur begrijpen en gebruiken.</span>
+                </span>
+                </li>
+                <li>
+                <span>
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-paw fa-stack-2x" aria-hidden="true"></i>
+                    </span>
+                    <span>Gedrag van honden en paarden herkennen en mee leren omgaan, leren paardrijden.</span>
+                </span>
+                
+                </li>
+                <li>
+
+                <span>
+                    <span class="fa-stack fa-lg">
+                        <i class="fa fa-sign-language fa-stack-2x" aria-hidden="true"></i>
+                    </span>
+                    <span>De begeleiding is 1 op 1 en altijd samen met een kinderfysiotherpeute.</span>
+                </span>
+                </li>
+            </ul>
+        </div>
+        <div class="subsection">
+            <h2>Doelstellingen Stichting Buitenbeesten</h2>
+            <p id='doelp'>Kinderen die om welke reden dan ook niet passen binnen de setting van een regulier
+                manege/kinderboerderij, een plek te bieden om te leren omgaan met met allerlei boerderijdieren en de
+                daarbij horende verzoring.
+                De kernactiviteit is het zoveel als mogelijk zelfstandig maken van deze kinderen door een leeromgeving
+                aan te bieden met dieren in het algemeen en in het bijzonder met paarden, olv. gekwalificeerd personeel.
+            </p>
+        </div>
+        <div class="subsection">
+            <h2>Beleidsplan Stichting Buitenbeesten</h2>
+            <div class="mid-col clearfix">
+                <a href="./beleidsplan" class="box float-left"><i class="fa fa-file-text" aria-hidden="true"></i>
+                <span class="icon-caption">Beleidsplan</span></a>
+                <a href="./begroting" class="box float-right"><i style='font-size: 120px' class="fa fa-table" aria-hidden="true"></i>
+                <span style="margin-top: -20px" class="icon-caption">Begroten</span></a>
+
+
+            </div>
+
+        </div>
+    </div>
