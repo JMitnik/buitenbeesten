43ea9b485fe4b4b01ad7a377e8e5b757710edff6
Jonathan Mitnik <jmitnik@gmail.com>
2016-04-23 17:05:50 +0200

    Push changes 2

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -39,37 +39,7 @@
         </div>
         <!--end slidehow-->
     </div>
-    <div id="doelstelling" class="clearfix section">
-        <div class="subsection">
-            <h1>Waar staat de naam stichting BuitenBeesten voor?</h1>
-            <p>Kinderen die om welke reden dan ook niet passen binnen de setting van een regulier
-                manege/kinderboerderij, een plek te bieden om te leren omgaan met met allerlei boerderijdieren en de
-                daarbij horende verzoring. De kernactiviteit is het zoveel als mogelijk zelfstandig maken van deze
-                kinderen door een leeromgeving aan te bieden met dieren in het algemeen en in het bijzonder met paarden,
-                olv. gekwalificeerd personeel.</p>
-        </div>
-        <div class="subsection">
-            <h2>Doelstellingen Stichting Buitenbeesten</h2>
-            <p id='doelp'>Kinderen die om welke reden dan ook niet passen binnen de setting van een regulier
-                manege/kinderboerderij, een plek te bieden om te leren omgaan met met allerlei boerderijdieren en de
-                daarbij horende verzoring.
-                De kernactiviteit is het zoveel als mogelijk zelfstandig maken van deze kinderen door een leeromgeving
-                aan te bieden met dieren in het algemeen en in het bijzonder met paarden, olv. gekwalificeerd personeel.
-            </p>
-        </div>
-        <div class="subsection">
-            <h2>Beleidsplan Stichting Buitenbeesten</h2>
-            <div class="mid-col clearfix">
-                <a href="./beleidsplan" class="box float-left"><i class="fa fa-file-text" aria-hidden="true"></i>
-                <span class="icon-caption">Beleidsplan</span></a>
-                <a href="./begroting" class="box float-right"><i style='font-size: 120px' class="fa fa-table" aria-hidden="true"></i>
-                <span class="icon-caption">Begroten</span></a>
-
-
-            </div>
-
-        </div>
-    </div>
+    <?php @include 'doelstellingen.php' ?>
 
 <div id="contact" class="section">
      <div id="infodiv">
