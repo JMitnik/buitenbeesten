af7427f18e007b53f0e21267f5810557a2925089
Jonathan Mitnik <JMitnik@gmail.com>
2017-06-29 18:27:25 +0200

    Current State of BB
    Sucky code

--- financieel.php
+++ financieel.php
@@ -1,13 +1,29 @@
 <div id="financieel" class="section clearfix">
    <h2>Financiële verantwoording</h2>
-   <h3>Jaarrekening 2015</h3>
-   <div class="fintable">
-<div class="datagrid"><table>
-<thead><tr><th>Baten & Lasten</th><th>Activa & Passiva</th></tr></thead>
-<tbody><tr><td>Baten: Nihil</td><td>Activa: Nihil</td></tr>
-<tr class="alt"><td>Lasten: Nihil</td><td>Passive: Nihil</td></tr>
-</tbody>
-</table></div>
+   <div>
+    <div class="clearfix" style="margin-bottom: 30px">
+      <div class="w50 dib float-left">
+        <h3>Jaarrekening 2015</h3>
+        <div class="fintable">
+          <div class="datagrid">
+            <table>
+              <thead><tr><th>Baten & Lasten</th><th>Activa & Passiva</th></tr></thead>
+              <tbody>
+                <tr><td>Baten: Nihil</td><td>Activa: Nihil</td></tr>
+                <tr class="alt"><td>Lasten: Nihil</td><td>Passive: Nihil</td></tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="w50 dib float-left">
+        <h3>Jaarrekening 2016</h3>
+        <a href="./assets/files/jaarrekening-2016.pdf" class="box subtle-hover block">
+          <i class="fa fa-eur block" aria-hidden="true"> </i>
+          <span class="block unhover">Bekijk de jaarrekening van 2016.</span>
+        </a>
+      </div>
+    </div>
     </div>
     <p>De stichting zijn eind november opgericht en in de weken tot eind december is er niets financieels gebeurd, er waren geen bankmutaties in 2015 want stichting kregen onze bankrekening pas begin 2016.</p>
     <br/>
